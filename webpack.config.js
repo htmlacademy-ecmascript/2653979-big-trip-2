3b513051b0d54b35db5ec097901d69dcc2422859
Vadim Voronin <vadim.voronin@gmail.com>
2025-11-14 15:50:41 +0300

    Насройка проекта

--- webpack.config.js
+++ webpack.config.js
@@ -3,30 +3,28 @@ const CopyPlugin = require('copy-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.js', // Точка входа
+  entry: './src/main.js',
   output: {
-    filename: 'bundle.[contenthash].js', // Имя бандла
-    path: path.resolve(__dirname, 'build'), // Директория для файлов сборки
-    clean: true, // Удаляем предыдущую сборку перед созданием новой
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
   },
-  devtool: 'source-map', // Генерируем карту исходного кода
-  plugins: [ // Подключаем плагины
+  devtool: 'source-map',
+  plugins: [
     new HtmlPlugin({
       template: 'public/index.html',
     }),
     new CopyPlugin({
-      patterns: [
-        {
-          from: 'public',
-          globOptions: {
-            ignore: ['**/index.html'],
-          },
+      patterns: [{
+        from: 'public',
+        globOptions: {
+          ignore: ['**/index.html'],
         },
-      ],
+      }],
     }),
   ],
   module: {
-    rules: [ // Добавляем лоадеры
+    rules: [
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
@@ -37,10 +35,6 @@ module.exports = {
           },
         },
       },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-    ],
-  },
+    ]
+  }
 };
